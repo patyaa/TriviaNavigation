3f564d021bd5f9334c9a0f25e04df99b514a9750
patyaa <b.patrik972531@gmail.com>
2021-06-29 12:40:11 +0200

    fixed code

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,66 +16,64 @@
 
 package com.example.android.navigation
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.os.Bundle
-import android.view.*
-import android.widget.Toast
-import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.core.app.ShareCompat
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.navArgs
 import com.example.android.navigation.databinding.FragmentGameWonBinding
+import android.content.pm.ResolveInfo
+import android.content.pm.PackageManager
+
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_game_won, container, false
-        )
+                inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+            view.findNavController().navigate(
+                    GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-
-//        Toast.makeText(
-//            context,
-//            "NumQuestions: ${args.numQuestions}, NumCorrect: ${args.numCorrect}",
-//            Toast.LENGTH_LONG
-//        ).show()
         setHasOptionsMenu(true)
-
         return binding.root
+    }
+
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return ShareCompat.IntentBuilder.from(activity!!)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.winner_menu, menu)
-        //check if the activity resolves
-        if(null == getShareIntent().resolveActivity(activity!!.packageManager)){
-            //hide the menu if it doesn't resolve
-            menu?.findItem(R.id.share)?.setVisible(false)
+        inflater.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
         }
     }
 
-    private fun getShareIntent(): Intent {
-        val args: GameWonFragmentArgs by navArgs()
-        return ShareCompat.IntentBuilder.from(activity!!)
-            .setText(getString(R.string.share_success_text, args.numQuestions, args.numCorrect))
-            .setType("text/plain")
-            .intent
-    }
-
-    private fun shareSuccess(){
-        startActivity(getShareIntent())
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item!!.itemId){
+        when (item.itemId) {
             R.id.share -> shareSuccess()
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -16,30 +16,30 @@
 
 package com.example.android.navigation
 
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        navController.addOnDestinationChangedListener { NavController, NavDestination, Bundle ->
-            if (NavDestination.id == NavController.graph.startDestination) {
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
             } else {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
